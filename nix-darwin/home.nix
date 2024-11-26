04d755e5e3372500a5138b2341ac6d6523afcce6
Gökay Yıldız <gokayyildiz0@gmail.com>
2024-11-26 10:31:05 +0300

    fix: refactor home.nix to remove absolute paths and enable Home Manager

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -1,36 +1,23 @@
 { config, pkgs, ... }:
 
-let
-  dotfiles = builtins.path {
-    path = "/Users/gokayyildiz/dotfiles"; # Absolute path to your dotfiles
-  };
-in
 {
   home.username = "gokayyildiz";
   home.homeDirectory = "/Users/gokayyildiz/";
   home.stateVersion = "23.05";
 
-  # Home Manager configuration for dotfiles
-  home.file = {
-    ".zshrc".source = "${config.home.homeDirectory}/dotfiles/zshrc/.zshrc";
-    ".config/wezterm".source = "${config.home.homeDirectory}/dotfiles/wezterm";
-    ".config/starship".source = "${config.home.homeDirectory}/dotfiles/starship";
-    ".config/zellij".source = "${config.home.homeDirectory}/dotfiles/zellij";
-    ".config/nvim".source = "${config.home.homeDirectory}/dotfiles/nvim";
-    ".config/nix".source = "${config.home.homeDirectory}/dotfiles/nix";
-    ".config/nix-darwin".source = "${config.home.homeDirectory}/dotfiles/nix-darwin";
-    ".config/tmux".source = "${config.home.homeDirectory}/dotfiles/tmux";
-  };
-
+  # Specify session variables
   home.sessionVariables = { };
 
+  # Extend session PATH
   home.sessionPath = [
     "/run/current-system/sw/bin"
     "$HOME/.nix-profile/bin"
   ];
 
+  # Enable Home Manager
   programs.home-manager.enable = true;
 
+  # Enable Zsh configuration
   programs.zsh = {
     enable = true;
     initExtra = ''
