3685093274502d4dd494ec5a677b80d1ee55e46f
Gökay Yıldız <gokayyildiz0@gmail.com>
2024-11-26 10:25:53 +0300

    fix: update home.nix to use a variable for dotfile paths and enable dotfile sources

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -1,38 +1,38 @@
 { config, pkgs, ... }:
 
+let
+  dotfiles = builtins.path {
+    path = "/Users/gokayyildiz/dotfiles"; # Absolute path to your dotfiles
+  };
+in
 {
   home.username = "gokayyildiz";
   home.homeDirectory = "/Users/gokayyildiz/";
   home.stateVersion = "23.05";
 
-  # Home Manager configuration for dotfiles
   home.file = {
-    #".zshrc".source = "${dotfiles}/zshrc/.zshrc";
-    #".config/wezterm".source = "${dotfiles}/wezterm";
-    #".config/starship".source = "${dotfiles}/starship";
-    #".config/zellij".source = "${dotfiles}/zellij";
-    #".config/nvim".source = "${dotfiles}/nvim";
-    #".config/nix".source = "${dotfiles}/nix";
-    #".config/nix-darwin".source = "${dotfiles}/nix-darwin";
-    #".config/tmux".source = "${dotfiles}/tmux";
+    ".zshrc".source = "${dotfiles}/zshrc/.zshrc";
+    ".config/wezterm".source = "${dotfiles}/wezterm";
+    ".config/starship".source = "${dotfiles}/starship";
+    ".config/zellij".source = "${dotfiles}/zellij";
+    ".config/nvim".source = "${dotfiles}/nvim";
+    ".config/nix".source = "${dotfiles}/nix";
+    ".config/nix-darwin".source = "${dotfiles}/nix-darwin";
+    ".config/tmux".source = "${dotfiles}/tmux";
   };
-  # Add custom session variables
+
   home.sessionVariables = { };
 
-  # Extend session PATH
   home.sessionPath = [
     "/run/current-system/sw/bin"
     "$HOME/.nix-profile/bin"
   ];
 
-  # Enable Home Manager as a program
   programs.home-manager.enable = true;
 
-  # Zsh configuration
   programs.zsh = {
     enable = true;
     initExtra = ''
-      # Add any additional configurations here
       export PATH=/run/current-system/sw/bin:$HOME/.nix-profile/bin:$PATH
       if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
         . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
