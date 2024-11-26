5ccee565f4ae1172626e49d51998990da77308a6
Gökay Yıldız <gokayyildiz0@gmail.com>
2024-11-26 12:22:03 +0300

    fix

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -1,30 +1,78 @@
 {
-  description = "Gokay's Darwin system flake";
+  description = "My Darwin system flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-     home-manager = {
-       url = "github:nix-community/home-manager";
-       inputs.nixpkgs.follows = "nixpkgs";
-     };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }: {
-    darwinConfigurations."Gokay's-Mac" = inputs.nix-darwin.lib.darwinSystem {
-      system = "aarch64-darwin"; # Architecture for Apple Silicon
-      modules = [
-        ./darwin-configuration.nix # System-specific configuration
-         inputs.home-manager.darwinModules.home-manager {
-           home-manager.useGlobalPkgs = true;
-           home-manager.useUserPackages = true;
-           home-manager.users.gokayyildiz = import ./home.nix;
-           home-manager.backupFileExtension = "backup";
-         }
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ 
+          pkgs.vim
+          pkgs.direnv
+          pkgs.sshs
+          pkgs.glow
+          pkgs.nushell
+          pkgs.carapace
+        ];
+      services.nix-daemon.enable = true;
+      nix.settings.experimental-features = "nix-command flakes";
+      programs.zsh.enable = true;  # default shell on catalina
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+      system.stateVersion = 4;
+      nixpkgs.hostPlatform = "aarch64-darwin";
+      security.pam.enableSudoTouchIdAuth = true;
+
+      users.users.gokayyildiz.home = "/Users/gokayyildiz";
+      home-manager.backupFileExtension = "backup";
+      nix.configureBuildUsers = true;
+      nix.useDaemon = true;
+
+      system.defaults = {
+        dock.autohide = true;
+        dock.mru-spaces = false;
+        finder.AppleShowAllExtensions = true;
+        finder.FXPreferredViewStyle = "clmv";
+        loginwindow.LoginwindowText = "devops-toolbox";
+        screencapture.location = "~/Pictures/screenshots";
+        screensaver.askForPasswordDelay = 10;
+      };
+
+      # Homebrew needs to be installed on its own!
+      homebrew.enable = true;
+      homebrew.casks = [
+        "font-meslo-lg-nerd-font"
+        "wezterm"
+      ];
+      homebrew.brews = [
+        "neovim"
+      ];
+    };
+  in
+  {
+    darwinConfigurations."Gokay's-Mac" = nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [ 
+	configuration
+        home-manager.darwinModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.gokayyildiz = import ./home.nix;
+        }
       ];
     };
 
+    # Expose the package set, including overlays, for convenience.
     darwinPackages = self.darwinConfigurations."Gokay's-Mac".pkgs;
   };
 }
