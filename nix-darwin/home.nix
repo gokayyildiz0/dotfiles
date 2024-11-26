4f3bdb000d58a01a77ca1709c87d9ef10bd52933
Gökay Yıldız <gokayyildiz0@gmail.com>
2024-11-26 13:25:53 +0300

    fix: remove darwin-configuration.nix and update flake.nix to integrate home-manager

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -1,9 +1,11 @@
+# home.nix
+# home-manager switch 
+
 { config, pkgs, ... }:
 
-
- {
+{
   home.username = "gokayyildiz";
-  home.homeDirectory = "/Users/gokayyildiz/";
+  home.homeDirectory = "/Users/gokayyildiz";
   home.stateVersion = "23.05"; # Please read the comment before changing.
 
 # Makes sense for user specific applications that shouldn't be available system-wide
@@ -13,39 +15,33 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-     ".zshrc".source = ~/dotfiles/zshrc/.zshrc;
-     #".config/wezterm".source = /dotfiles/wezterm;
+    # ".zshrc".source = ~/dotfiles/zshrc/.zshrc;
+    # ".config/wezterm".source = ~/dotfiles/wezterm;
     # ".config/skhd".source = ~/dotfiles/skhd;
-    # ".config/starship".source = /dotfiles/starship;
-    # ".config/zellij".source = /dotfiles/zellij;
-     #".config/nvim".source = /dotfiles/nvim;
-    # ".config/nix".source = /dotfiles/nix;
-    # ".config/nix-darwin".source = /dotfiles/nix-darwin;
-    # ".config/tmux".source = /dotfiles/tmux;
+    # ".config/starship".source = ~/dotfiles/starship;
+    # ".config/zellij".source = ~/dotfiles/zellij;
+    # ".config/nvim".source = ~/dotfiles/nvim;
+    # ".config/nix".source = ~/dotfiles/nix;
+    # ".config/nix-darwin".source = ~/dotfiles/nix-darwin;
+    # ".config/tmux".source = ~/dotfiles/tmux;
     # ".config/ghostty".source = ~/dotfiles/ghostty;
     # ".config/aerospace".source = ~/dotfiles/aerospace;
     # ".config/sketchybar".source = ~/dotfiles/sketchybar;
     # ".config/nushell".source = ~/dotfiles/nushell;
   };
 
+  home.sessionVariables = {
+  };
 
-
-  # Specify session variables
-  home.sessionVariables = { };
-
-  # Extend session PATH
   home.sessionPath = [
     "/run/current-system/sw/bin"
-    "$HOME/.nix-profile/bin"
+      "$HOME/.nix-profile/bin"
   ];
-
-  # Enable Home Manager
   programs.home-manager.enable = true;
-
-  # Enable Zsh configuration
   programs.zsh = {
     enable = true;
     initExtra = ''
+      # Add any additional configurations here
       export PATH=/run/current-system/sw/bin:$HOME/.nix-profile/bin:$PATH
       if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
         . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
