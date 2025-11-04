d7561b46edb935babb2337795f512d8fea8d0cf6
Gökay Yıldız <gokayyildiz0@gmail.com>
2025-11-04 18:42:52 +0300

    LazyVim installed

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,2 @@
-require("gokay.core")
-require("gokay.lazy")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- nvim/lua/gokay/core/init.lua
+++
@@ -1,3 +0,0 @@
--- Load core settings and keymaps
-require("gokay.core.options")
-require("gokay.core.keymaps")
